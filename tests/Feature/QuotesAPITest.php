cf52630e2f328a3ee9b8a49ba3e0df9470edfff4
Callum Ormsby <callum@hirestreetuk.com>
2024-04-28 17:46:10 +0100

    Add a feature test for the refreshing functionality

--- tests/Feature/QuotesAPITest.php
+++ tests/Feature/QuotesAPITest.php
@@ -21,6 +21,25 @@ class QuotesAPITest extends TestCase
     }
 
     /**
+     * Test that after patching to the refresh endpoint, we start receiving new quotes.
+     */
+    public function test_quotes_are_refreshed(): void
+    {
+        // Hit the fetch endpoint to form quote cache.
+        $initialResponse = $this->withoutMiddleware()->get(route('quotes.kanye.get'));
+        $initialQuotes = json_decode($initialResponse->getContent(), true);
+
+        // Hit the reset endpoint to force new quotes to be fetched.
+        $this->withoutMiddleware()->patch(route('quotes.kanye.reset'));
+
+        // Hit the fetch endpoint again to see new results.
+        $updatedResponse = $this->withoutMiddleware()->get(route('quotes.kanye.get'));
+        $updatedQuotes = json_decode($updatedResponse->getContent(), true);
+
+        $this->assertNotSame($updatedQuotes, $initialQuotes);
+    }
+
+    /**
      * Test the Kanye endpoint is inaccessible when using an invalid token.
      */
     public function test_getting_kanye_quotes_without_valid_token_is_impossible(): void
