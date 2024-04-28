3e6344c512190ca7a381adbdb238d4573086925f
Callum Ormsby <callum@hirestreetuk.com>
2024-04-28 13:16:48 +0100

    Cache quotes for speed

--- app/Http/Controllers/QuotesController.php
+++ app/Http/Controllers/QuotesController.php
@@ -3,16 +3,12 @@
 namespace App\Http\Controllers;
 
 use \App\QuotesFetcher\QuotesFetcher;
+use Illuminate\Support\Facades\Cache;
 
 class QuotesController extends Controller {
     public function getKanyeQuotes()
     {
-        $quotes = [];
-
-        for ($i = 0; $i < 5; $i++) {
-            $quotes[] = QuotesFetcher::driver('kanye')->fetch();
-        }
-
+        $quotes = Cache::rememberForever('quotes.kanye.endpoint', fn () => QuotesFetcher::driver('kanye')->fetchMany(5));
         return response()->json($quotes);
     }
 }
