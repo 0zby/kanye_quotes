41fdf3471448900038f05df6be9e9a7ad15cd036
Callum Ormsby <callum@hirestreetuk.com>
2024-04-29 08:52:43 +0100

    Use better default code in exception

--- app/QuotesFetcher/ThirdPartyAPIUnavailableException.php
+++ app/QuotesFetcher/ThirdPartyAPIUnavailableException.php
@@ -3,10 +3,11 @@
 namespace App\QuotesFetcher;
 
 use Exception;
+use Illuminate\Http\Response;
 
 class ThirdPartyAPIUnavailableException extends Exception
 {
-    public function __construct($message = 'Third-party API is unavailable', $code = 0, Exception $previous = null)
+    public function __construct($message = 'Third-party API is unavailable', $code = Response::HTTP_SERVICE_UNAVAILABLE, Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
