a784818c2c702d7eb2c6fb9c6bc5bfe7e1f5028a
Callum Ormsby <callum@hirestreetuk.com>
2024-04-29 08:53:50 +0100

    Add missing doc block

--- app/QuotesFetcher/QuotesFetcherInterface.php
+++ app/QuotesFetcher/QuotesFetcherInterface.php
@@ -11,5 +11,11 @@ interface QuotesFetcherInterface
      */
     public function fetch(): string;
 
+    /**
+     * Fetch multiple quotes from an external resource and return them.
+     *
+     * @param int $numQuotes The number of quotes to fetch.
+     * @return array<string> An array of quotes from the external resource.
+     */
     public function fetchMany(int $numQuotes): array;
 }
