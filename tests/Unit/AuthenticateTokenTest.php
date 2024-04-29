64e123b395c13e77bbc209707c1d412ad117a610
Callum Ormsby <callum@hirestreetuk.com>
2024-04-29 09:21:39 +0100

    Test for case where there's no token provided

--- tests/Unit/AuthenticateTokenTest.php
+++ tests/Unit/AuthenticateTokenTest.php
@@ -9,7 +9,10 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthenticateTokenTest extends TestCase
 {
-    public function test_valid_token()
+    /**
+     * Test that when the middleware receives a request with a valid token, it proceeds to the next middleware.
+     */
+    public function test_valid_token(): void
     {
         $validToken = env('API_TOKEN');
         $middleware = new AuthenticateToken();
@@ -25,12 +28,30 @@ class AuthenticateTokenTest extends TestCase
         $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
     }
 
-    public function test_invalid_token()
+    /**
+     * Test that when the middleware receives a request with an invalid token, it returns an unauthorized response.
+     */
+    public function test_invalid_token(): void
     {
         $middleware = new AuthenticateToken();
 
         $request = Request::create('/', 'GET');
         $request->headers->set('Authorization', 'Bearer INVALID_TOKEN');
+
+        $response = $middleware->handle($request, function () {});
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    /**
+     * Test that when the middleware receives a request without a token, it returns an unauthorized response.
+     */
+    public function test_null_token(): void
+    {
+        $middleware = new AuthenticateToken();
+
+        $request = Request::create('/', 'GET');
 
         $response = $middleware->handle($request, function () {});
 
